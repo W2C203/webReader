829c0cc6a573139c0b115da31f62a5dd33bae32d
Joe <347126391@qq.com>
2015-08-03 19:47:15 +0800

    最开始的一点进展

--- app.js
+++ app.js
@@ -5,8 +5,15 @@
 var express = require('express');
 var test = require('./server/file/read_error_code_test');
 var app = express();
+var userDao = require('./server/db/userDao');
 
 app.get('/', test.readErrorCode);
 
-app.listen(3000);
+app.get('/query', function(req, res, next) {
+    userDao.verifyByNamePassword(req, res, next);
+});
+
+app.listen(3000,function(){
+    console.log("listening on 3000")
+});
 
