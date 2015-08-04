db3be75c34f1aad7632e298763f6e8b910848c8e
SeanLeeCai <576719032@qq.com>
2015-08-04 22:16:00 +0800

    添加一些事件监听在control.js里面

---
+++ public/scripts/control.js
@@ -0,0 +1,29 @@
+/**
+ * Created by seanlee on 15-8-4.
+ */
+$("#menuBtn").on('mouseover', function () {
+    $(this).addClass('selected');
+    $("#menuList").show();
+});
+$("#menuBtn").on('mouseout', function () {
+    $(this).removeClass('selected');
+    $("#menuList").hide();
+});
+$("#icon").on('click', function () {
+    if($(this).attr("title") == "hide"){
+        $(this).attr("title","show");
+        $(this).addClass('selected');
+    } else if($(this).attr("title") == "show"){
+        $(this).attr("title","hide");
+        $(this).removeClass('selected');
+    }
+    $("#leftSide div").toggle();
+});
+$("button[title^='p']").on('click', function () {
+    alert("prev");
+});
+$("button[title^='n']").on('click', function () {
+    alert("next");
+});/**
+ * Created by seanlee on 15-8-4.
+ */
