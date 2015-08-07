935ab6c73d81f209e234257dba7f05c0693db370
Joe <347126391@qq.com>
2015-08-07 11:04:56 +0800

    左边功能基本完成

--- public/scripts/verify.js
+++ public/scripts/verify.js
@@ -14,10 +14,10 @@ $('#submit').on('click', function (event) {
     $.post("/verify/query", data, function (text, status) {
         //一共会有3种成功响应   请填写帐号/密码(这个正常是不会有的) 帐号或密码错误 登录成功
         if (JSON.stringify(text) == '"登录成功"') {
-            $('#form').hide();
             var name = data.split('&')[0].split('=');
             //例： user_name=admin&password=admin  抽取用户名 这里的策略有待修改
             name = name[name.length - 1];             //这个是抽取的用户名 到时找个地方放
+            changeInformation(name);
             showOrderBooks(name);
         } else {
             alert(JSON.stringify(text));
@@ -52,3 +52,39 @@ function showOrderBooks(name) {
     })
     event.preventDefault();//阻止默认行为
 }
+/**
+ * 用于跳转到已登录效果
+ */
+function changeInformation(name){
+    $('#information').html('welcome');
+    $('#user_name').addClass('hide');
+    $('#password').addClass('hide');
+
+    $('#show_name').html(name).removeClass('hide');
+
+    $('#submit').addClass('hide');
+    $('#logout').removeClass('hide');
+}
+/**
+ * 退出登录
+ */
+$('#logout').on('click', function () {
+    $('#information').html('登录');
+
+
+    $('#user_name').val('').removeClass('hide');
+    $('#password').val('').removeClass('hide');
+
+    $('#show_name').addClass('hide');
+
+    $('#submit').removeClass('hide');
+    $('#logout').addClass('hide');
+
+    $('#pBeforeBooks').html('登录后查看购买书目');
+    $('#books').removeClass('ribbons');
+    $('#books *').remove();
+
+})
+$('#mall').on('click', function () {
+    window.open('http://192.168.69.17:8080/mall','_self');
+})
